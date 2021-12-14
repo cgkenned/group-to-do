73e5139295f237d91bb529e067f931dddcb2b342
Ryan Moses <ryanchristophermoses@gmail.com>
2021-12-13 23:17:38 -0500

    Added Lucy's remove item functionality -push remove items into an array -Appended new list items to Completed List

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,38 +1,47 @@
 let ul = document.querySelector('#to-do-list');
 
+let done = document.querySelector('#completed-list');
+
 let btn = document.querySelector('.addItem');
 
 let listItems = ul.children;
 
 function addToDo(input) {
-    let li = document.createElement('li');
-    let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'checkbox';
-    checkbox.value = 'false';
+  let li = document.createElement('li');
+  let checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'checkbox';
+  checkbox.value = 'false';
 
-    li.appendChild(checkbox);
-    let span = document.createElement('span');
-    li.appendChild(span);
+  li.appendChild(checkbox);
+  let span = document.createElement('span');
+  li.appendChild(span);
 
-    span.innerText = input;
-    ul.appendChild(li);
+  li.addEventListener('click', removeLi);
+
+  span.innerText = input;
+  ul.appendChild(li);
 }
 
-function removeItem() {
-    for (i = 0; i < listItems.length; i++) {
-        listItems[i].addEventListener('click', function (event) {
-            event.currentTarget.style.textDecoration = 'line-through';
-            setTimeout(() => { listItems[i].remove(); }, 1000);
-        });
-    }
+const completedArray = [];
+
+function removeLi(e) {
+  completedArray.push(e.target.innerText);
+  e.currentTarget.style.textDecoration = 'line-through';
+  setTimeout(() => {
+    e.target.remove();
+  }, 1000);
+
+  let li = document.createElement('li');
+  for(i = 0; i < completedArray.length; i++) {
+      li.innerText = completedArray[i];
+      done.appendChild(li);
+  }
 }
 
 btn.addEventListener('click', function () {
-    let inputElement = document.querySelector('#inputBox');
-    let input = inputElement.value;
-    addToDo(input);
-    inputElement.value = '';
+  let inputElement = document.querySelector('#inputBox');
+  let input = inputElement.value;
+  addToDo(input);
+  inputElement.value = '';
 });
-
-ul.addEventListener('mouseenter', removeItem);

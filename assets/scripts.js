38ac08d21ad13e05aec77687aaf465ff2c36e56b
cgkenned <kennedy.carolinagrace@gmail.com>
2021-12-13 21:52:41 -0500

    modifying js

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,35 +2,37 @@ let ul = document.querySelector('#to-do-list');
 
 let btn = document.querySelector('.addItem');
 
-let listItems = document.querySelectorAll('li');
+let listItems = ul.children;
 
 function addToDo(input) {
-  let li = document.createElement('li');
-  let checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.className = 'checkbox';
-  checkbox.value = 'false';
+    let li = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+    checkbox.value = 'false';
 
-  li.appendChild(checkbox);
-  let span = document.createElement('span');
-  li.appendChild(span);
+    li.appendChild(checkbox);
+    let span = document.createElement('span');
+    li.appendChild(span);
 
-  span.innerText = input;
-  ul.appendChild(li);
+    span.innerText = input;
+    ul.appendChild(li);
 }
 
-btn.addEventListener('click', function() {
+function removeItem() {
+    for (i = 0; i < listItems.length; i++) {
+        listItems[i].addEventListener('click', function (event) {
+            event.currentTarget.style.textDecoration = 'line-through';
+            setTimeout(() => { listItems[i].remove(); }, 1000);
+        });
+    }
+}
+
+btn.addEventListener('click', function () {
     let inputElement = document.querySelector('#inputBox');
     let input = inputElement.value;
     addToDo(input);
     inputElement.value = '';
 });
 
-
-let i;
-for (i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener('click', function(event) {
-        event.currentTarget
-        this.style.display = 'none';
-    });
-}
+ul.addEventListener('mouseenter', removeItem);
